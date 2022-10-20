69ae11cf6e1fe5b8361fcca0330097fbcc5cdf27
XisosongZhai <60103954+XisosongZhai@users.noreply.github.com>
2022-10-20 14:51:07 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from waybackmachine import waybackurl
+from flask import Flask,request,jsonify
+
+# http://10.8.1.104:2020/?input=rel8ed.to&year=2021
+
+app=Flask(__name__)
+
+@app.route('/')
+
+
+def index():
+
+
+
+ 
+
+
+    data=request.args
+    url=data.get('input')
+    year=data.get('year')
+
+
+
+    # raise Exception('page error')
+
+    data=waybackurl(url,year)
+    
+    return (data)
+    # return df.to_html()
+    # return resp
+
+app.run(host='0.0.0.0',port='2020')
+
+
